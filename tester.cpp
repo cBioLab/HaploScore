df46ce223a5535c691a14d16fa271d9f29bb879d
DAyama <taiki_yamada@akane.waseda.jp>
2018-12-11 21:27:02 +0900

    modify

--- tester.cpp
+++ tester.cpp
@@ -9,7 +9,7 @@ int main(){
 
   const int numofTest = 1000;
 
-  for(int p = 1; p < 10; p++){
+  for(double p = 0.1; p < 1; p += 0.1){
     system("rm -rf testdata");
     system("rm -rf results");
     system("mkdir testdata");
@@ -23,8 +23,9 @@ int main(){
       string haplofile = "./testdata/haplo" + to_string(i) + ".csv";
 
       system(("./generator " + to_string(i)).c_str());
-      system(("./path -H " + haploSetfile1 + " -h " + haplofile + " -s " + haploScorefile1 + " -p " + to_string((double)0.1*p)).c_str());
-      system(("./path -H " + haploSetfile2 + " -h " + haplofile + " -s " + haploScorefile2 + " -p " + to_string((double)0.1*p)).c_str());
+      system(("./path -H " + haploSetfile1 + " -h " + haplofile + " -s " + haploScorefile1 + " -p " + to_string(p)).c_str());
+      system(("./path -H " + haploSetfile2 + " -h " + haplofile + " -s " + haploScorefile2 + " -p " + to_string(p)).c_str());
+      dbg(to_string(p));
       ifstream inputfile1(haploScorefile1), inputfile2(haploScorefile2);
       double haploScore1, haploScore2;
       inputfile1 >> haploScore1; inputfile1.close();
