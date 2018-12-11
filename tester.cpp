c54a7ffb73957a359fb773d7d85458b504389989
DAyama <taiki_yamada@akane.waseda.jp>
2018-12-11 15:33:33 +0900

    modify tester

--- tester.cpp
+++ tester.cpp
@@ -8,9 +8,9 @@ int main(){
   system("mkdir testdata");
   const int numofTest = 10;
   for(int i = 1; i <= numofTest; i++){
-    string haploSetfile1 = ".testdata/haploSet" + to_string(i) + ".csv";
-    string haploSetfile2 = ".testdata/haploSet_cut" + to_string(i) + ".csv";
-    string haplofile = ".testdata/haplo" + to_string(i) + ".csv";
+    string haploSetfile1 = "./testdata/haploSet" + to_string(i) + ".csv";
+    string haploSetfile2 = "./testdata/haploSet_cut" + to_string(i) + ".csv";
+    string haplofile = "./testdata/haplo" + to_string(i) + ".csv";
 
     system(("./generator " + to_string(i)).c_str());
     system(("./path -H " + haploSetfile1 + " -h " + haplofile + " >> result.csv").c_str());
