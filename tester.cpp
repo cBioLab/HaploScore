b983197a223980b081e035410d163d4fb3200929
DAyama <taiki_yamada@akane.waseda.jp>
2018-12-11 16:12:32 +0900

    modify path.cpp

--- tester.cpp
+++ tester.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
+#include <fstream>
+#include <math.h>
 using namespace std;
 
 #define dbg(x) cerr << #x << "=" << x << endl
 
 int main(){
 
+  const int numofTest = 10;
+
   system("mkdir testdata");
   system("mkdir results");
-  const int numofTest = 10;
+  ofstream outputfile("result.csv");
   for(int i = 1; i <= numofTest; i++){
     string haploSetfile1 = "./testdata/haploSet" + to_string(i) + ".csv";
     string haploSetfile2 = "./testdata/haploSet_cut" + to_string(i) + ".csv";
@@ -18,7 +22,15 @@ int main(){
     system(("./generator " + to_string(i)).c_str());
     system(("./path -H " + haploSetfile1 + " -h " + haplofile + " -s " + haploScorefile1).c_str());
     system(("./path -H " + haploSetfile2 + " -h " + haplofile + " -s " + haploScorefile2).c_str());
+    ifstream inputfile1(haploScorefile1), inputfile2(haploScorefile2);
+    double haploScore1, haploScore2;
+    inputfile1 >> haploScore1; inputfile1.close();
+    inputfile2 >> haploScore2; inputfile2.close();
+    double arglog = log(haploScore2 / haploScore1);
+    outputfile<<arglog<<",";
   }
+  outputfile<<endl;
+  outputfile.close();
 
   return 0;
 }
