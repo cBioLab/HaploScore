0522b37f74f4591e1f321528f993db237a48bfd4
DAyama <taiki_yamada@akane.waseda.jp>
2018-12-11 15:29:22 +0900

    modify tester

--- tester.cpp
+++ tester.cpp
@@ -1,8 +1,4 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
-#include <random>
-#include <fstream>
 using namespace std;
 
 #define dbg(x) cerr << #x << "=" << x << endl
@@ -12,9 +8,9 @@ int main(){
   system("mkdir testdata");
   const int numofTest = 10;
   for(int i = 1; i <= numofTest; i++){
-    string haploSetfile1 = "haploSet" + to_string(i) + ".csv";
-    string haploSetfile2 = "haploSet_cut" + to_string(i) + ".csv";
-    string haplofile = "haplo" + to_string(i) + ".csv";
+    string haploSetfile1 = ".testdata/haploSet" + to_string(i) + ".csv";
+    string haploSetfile2 = ".testdata/haploSet_cut" + to_string(i) + ".csv";
+    string haplofile = ".testdata/haplo" + to_string(i) + ".csv";
 
     system(("./generator " + to_string(i)).c_str());
     system(("./path -H " + haploSetfile1 + " -h " + haplofile + " >> result.csv").c_str());
