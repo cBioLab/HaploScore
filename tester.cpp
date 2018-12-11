d10cbec6b151a9ddb709287ed3bfec8b4924d130
DAyama <taiki_yamada@akane.waseda.jp>
2018-12-11 20:52:46 +0900

    modify

--- tester.cpp
+++ tester.cpp
@@ -23,8 +23,8 @@ int main(){
       string haplofile = "./testdata/haplo" + to_string(i) + ".csv";
 
       system(("./generator " + to_string(i)).c_str());
-      system(("./path -H " + haploSetfile1 + " -h " + haplofile + " -s " + haploScorefile1 + "-H" + to_string((double)0.1*p)).c_str());
-      system(("./path -H " + haploSetfile2 + " -h " + haplofile + " -s " + haploScorefile2 + "-H" + to_string((double)0.1*p)).c_str());
+      system(("./path -H " + haploSetfile1 + " -h " + haplofile + " -s " + haploScorefile1 + " -p " + to_string((double)0.1*p)).c_str());
+      system(("./path -H " + haploSetfile2 + " -h " + haplofile + " -s " + haploScorefile2 + " -p " + to_string((double)0.1*p)).c_str());
       ifstream inputfile1(haploScorefile1), inputfile2(haploScorefile2);
       double haploScore1, haploScore2;
       inputfile1 >> haploScore1; inputfile1.close();
