ea712d85c8c6e008ed40fc0c797b0a106e2bae95
DAyama <taiki_yamada@akane.waseda.jp>
2018-12-11 21:28:10 +0900

    modify

--- tester.cpp
+++ tester.cpp
@@ -25,7 +25,6 @@ int main(){
       system(("./generator " + to_string(i)).c_str());
       system(("./path -H " + haploSetfile1 + " -h " + haplofile + " -s " + haploScorefile1 + " -p " + to_string(p)).c_str());
       system(("./path -H " + haploSetfile2 + " -h " + haplofile + " -s " + haploScorefile2 + " -p " + to_string(p)).c_str());
-      dbg(to_string(p));
       ifstream inputfile1(haploScorefile1), inputfile2(haploScorefile2);
       double haploScore1, haploScore2;
       inputfile1 >> haploScore1; inputfile1.close();
