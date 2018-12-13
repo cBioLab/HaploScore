5885ae388346b31ac63fc13289ada1778d07bd11
DAyama <taiki_yamada@akane.waseda.jp>
2018-12-13 22:33:50 +0900

    modify plot

--- plot.py
+++ plot.py
@@ -4,11 +4,13 @@ import matplotlib.pyplot as plt
 for i in range(1,10):
   data = np.loadtxt("result_cut"+str(i)+".csv",delimiter=",")
 
+  plt.clf()
   plt.hist(data,50)
   plt.savefig("figure_cut"+str(i)+".png");
 
 for i in range(1,10):
   data = np.loadtxt("result_shf"+str(i)+".csv",delimiter=",")
 
+  plt.clf()
   plt.hist(data,50)
   plt.savefig("figure_shf"+str(i)+".png");
