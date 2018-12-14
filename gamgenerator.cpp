a323cf77575aee00f24d99f23cd78909ff38367b
DAyama <taiki_yamada@akane.waseda.jp>
2018-12-14 13:49:53 +0900

    add csv2json.py

--- gamgenerator.cpp
+++ gamgenerator.cpp
@@ -4,36 +4,128 @@
 #include <random>
 #include <fstream>
 #include <cassert>
+#include <map>
+#include <set>
 using namespace std;
 
 #define dbg(x) cerr << #x << "=" << x << endl
+//#define DEBUG
 
 struct GAMelem{
-  int node;
-  int basenum;
-
+  int node, basenum;
+  string base;
 };
+bool operator==(const GAMelem& a, const GAMelem& b){
+  if(a.node == b.node && a.base == b.base)return true;
+  else return false;
+}
 typedef vector<GAMelem> Haplo;
 typedef vector<Haplo> GAM;
+
+vector<string> split(const string &s, char c1, char c2) {
+  vector<string> elems;
+  string item;
+  int lev = 0;
+  for (char ch: s) {
+    if (ch == c1) {
+      lev++;
+    } else if(ch == c2) {
+      elems.push_back(item);
+      lev--;
+      item.clear();
+    } else if(lev > 0){
+      item += ch;
+    }
+  }
+  return elems;
+}
 
 string getfileName(string prf, string suf, int fileidx){
   return "./testdata" + prf + to_string(fileidx) + suf;
 }
 
-void outputGAM(const &GAM hs, string filename){
+GAM inputGAM(){
+  ifstream inputfile("test.txt");
+
+  string pathinfo, haploinfo;
+  Haplo inputhaplo;
+  getline(inputfile, pathinfo);
+  getline(inputfile, haploinfo);
+  inputfile.close();
+  map<int, int> node2idx;
+  GAM inputgam;
+
+  // get GAM data
+  haploinfo = haploinfo.substr(1,haploinfo.size()-2);
+  vector<string> gs = split(haploinfo, '(', ')');
+  for(string str : gs){
+    int node = 0, basenum;
+    string base;
+    int idx = 0;
+    while(idx < (int)str.size() && str[idx] != ','){
+      node *= 10;
+      node += str[idx++] - '0';
+    }
+    idx += 3;
+    while(idx < (int)str.size() && str[idx] != '\''){
+      base += str[idx++];
+    }
+    basenum = base.size();
+    inputhaplo.push_back((GAMelem){node, basenum, base});
+    node2idx[node] = inputhaplo.size() - 1;
+  }
+
+  // get path info
+  pathinfo = pathinfo.substr(1,pathinfo.size()-2);
+  vector<string> hs = split(pathinfo, '[', ']');
+  for(string str : hs){
+    int idx = 0;
+    Haplo path; 
+    while(1){
+      int node = 0;
+      while(idx < (int)str.size() && isdigit(str[idx])){
+        node *= 10;
+        node += str[idx++] - '0';
+      }
+      path.push_back(inputhaplo[node2idx[node]]);
+      if(idx == (int)str.size() )break;
+      while(idx < (int)str.size() && !isdigit(str[idx])){
+        idx++;
+      }
+    }
+    inputgam.push_back(path);
+  }
+
+  return inputgam;
+}
+
+void outputGAM(const GAM& hs, string filename){
   ofstream ofs(filename);
   for(Haplo h : hs){
-    for(GAMelem g : h){
-      ofs << g.node + 1 << ","; // 1-indexed
+    ofs << h[0].node;
+    for(int i = 1; i < (int)h.size(); i++){
+      ofs << "," << h[i].node;
     }
-    ofs << "0" << endl; // END-MARKER
+    ofs << endl;
   }
+  ofs << "0" << endl; // END-MARKER
+
+  set<pair<int,string> > seqset;
+  for(Haplo h : hs){
+    for(GAMelem e : h){
+      seqset.insert(pair<int,string>(e.node,e.base));
+    }
+  }
+  for(pair<int,string> p : seqset){
+    ofs << p.first << "," << p.second << endl;
+  }
+  ofs << "0" << endl;
+
   ofs.close();
 }
 
 Haplo createHaplo(const GAM& gam){
   int haploNum = gam.size();
-  int haploLen = gam[0].size();
   random_device rnd;
   mt19937 mt(rnd());
   uniform_int_distribution<> randh(0, haploNum - 1);
@@ -43,25 +135,29 @@ Haplo createHaplo(const GAM& gam){
   double piC = 0.7; // current piC
   Haplo resultHaplo;
 
-  for(int i = 0; i < haploLen; i++){
-    //resultHaplo.push_back(crtNode);
-    vector<int> canditate;
+  for(int i = 0; ; ){
+    resultHaplo.push_back((GAMelem){crtNode, gam[crtHaplo][i].basenum, gam[crtHaplo][i].base});
+    vector<pair<int,int> > canditate;
     for(int j = 0; j < haploNum; j++){
-      if(j == crtHaplo || gam[j][i].node != crtNode) continue;
-      canditate.push_back(j);
-    }
-    if(canditate.size() > 0) {
-      double piR = (1.0 - piC) / (double) canditate.size();
-      for(int cnt = 0; cnt < gam[crtHaplo][i].basenum; cnt++){
-        double p = (double) rand100(mt) / 100.0;
-        if(p >= piC){
-          int nxtHaploidx = (int)floor((p - piC) / piR);
-          nxtHaploidx = min(nxtHaploidx, (int)canditate.size() - 1);
-          crtHaplo = canditate[nxtHaploidx];
-        }
+      for(int k = 0; k < (int)gam[j].size(); k++){
+        if(gam[j][k].node != crtNode) continue;
+        canditate.push_back(pair<int,int>(j,k));
+        break;
       }
     }
-    if(i + 1 < haploLen)crtNode = gam[crtHaplo][i+1].node;
+    double piR = (1.0 - piC) / (double) canditate.size();
+    for(int cnt = 0; cnt < gam[crtHaplo][i].basenum; cnt++){
+      double p = (double) rand100(mt) / 100.0;
+      if(p >= piC){
+        int nxtHaploidx = (int)floor((p - piC) / piR);
+        nxtHaploidx = min(nxtHaploidx, (int)canditate.size() - 1);
+        crtHaplo = canditate[nxtHaploidx].first;
+        i = canditate[nxtHaploidx].second;
+      }
+    }
+    if(i + 1 < (int)gam[crtHaplo].size()) {
+      crtNode = gam[crtHaplo][i+1].node; i++;
+    } else break;
   }
 
   return resultHaplo;
@@ -69,28 +165,54 @@ Haplo createHaplo(const GAM& gam){
 
 GAM gamGenerate(const GAM& gam){
   GAM resultSet;
-  for(int i = 0; i < (int) gam.size(); i++){
-    resultSet.push_back(createHaplo(gam));
+  while(1){
+    Haplo newh = createHaplo(gam);
+    bool ok = true;
+    for(Haplo h : gam){
+      if(newh == h){
+        ok = false;
+        break;
+      }
+    }
+    if(ok){
+      resultSet.push_back(newh);
+      break;
+    }
   }
   return resultSet;
 }
 
-GAM getInput(){
-  GAM inputgam;
-
-  string s;
-  while(getline(cin,s)){
-    int node, basenum;
-    sscanf(s.c_str(), "%d,%d", &node, &basenum);
-    //inputgam.push_back((Haplo){node, basenum});
-  }
-  return inputgam;
-}
-
 int main(int argc, char *argv[]){
-  GAM inputgam = getInput();
+  GAM inputgam = inputGAM();
+#ifdef DEBUG
+  // for DEBUG
+  for(Haplo h : inputgam){
+    cerr << " ===== " << endl;
+    int len = 0;
+    for(GAMelem g : h){
+      cerr << g.node << " " ;
+      len += g.basenum;
+    }
+    cerr << endl;
+    dbg(len);
+  }
+  cerr<<endl;
+#endif
+
   GAM resultgam = gamGenerate(inputgam);
-  outputGAM(resultgam, getfileName());
+#ifdef DEBUG
+  // for DEBUG
+  for(Haplo h : resultgam){
+    cerr << " ===== " << endl;
+    int len = 0;
+    for(GAMelem g : h){
+      cerr << g.node << " ";
+      len += g.basenum;
+    }
+    cerr << endl;
+  }
+#endif
+  outputGAM(resultgam, "test.csv");
 
   return 0;
 }
